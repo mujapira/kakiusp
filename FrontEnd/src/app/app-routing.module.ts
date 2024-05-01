5713475370c92ff96a6586a84630c99041a4f72c
mujapira <malmasmello2@gmail.com>
2024-05-01 16:10:31 -0300

    sidebar + darkmode

--- FrontEnd/src/app/app-routing.module.ts
+++ FrontEnd/src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
+import { HomeComponent } from './pages/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { FinancialComponent } from './pages/financial/financial.component';
+import { NotfoundComponent } from './pages/notfound/notfound.component';
+import { routes } from './routes';
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
---
+++ FrontEnd/src/app/pages/financial/financial.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-financial',
+  templateUrl: './financial.component.html',
+})
+export class FinancialComponent {
+
+}
